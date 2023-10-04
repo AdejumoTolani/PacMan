29b48473276d8ae4d2c5e2f8890d34492ab31c12
Adejumo Tolani <www.adejumo.omotolani@gmail.com>
2023-10-04 21:37:23 +0100

    commit message

--- PACMAN/netlify/functions/index.js
+++ PACMAN/netlify/functions/index.js
@@ -65,4 +65,5 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-export const handler = serverless(app)
+// export const handler = serverless(app)
+module.exports.handler = serverless(app);
