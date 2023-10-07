5ee9aae2804e0ba34ef9423c242331489af3f265
Adejumo Tolani <www.adejumo.omotolani@gmail.com>
2023-10-07 20:21:08 +0100

    removed backend to focus on just front

--- PACMAN/Models/Leaderboard.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const leaderboardSchema = new mongoose.Schema({
-  name: String,
-  score: String,
-}, {timestamps:true});
-
-module.exports = mongoose.model("Leaderboard", leaderboardSchema);
